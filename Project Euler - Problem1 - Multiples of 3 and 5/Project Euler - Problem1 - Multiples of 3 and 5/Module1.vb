7b6ab2ae626607f982cd837a1cf2f036eeb7c9df
Huzaifa-N <147554901+Huzaifa-N@users.noreply.github.com>
2023-10-13 21:40:13 +0100

    Add files via upload
    Problem 1 - Sum of multiples from 3 to 5

---
+++ Project Euler - Problem1 - Multiples of 3 and 5/Project Euler - Problem1 - Multiples of 3 and 5/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim total As Integer
+
+        For i = 1 To 999
+            If i Mod 3 = 0 Or i Mod 5 = 0 Then
+                total += i
+            End If
+        Next
+
+        Console.Write(total)
+
+
+        Console.ReadKey()
+    End Sub
+
+End Module
